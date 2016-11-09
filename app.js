1d477f81583415302531b551705a069b14a61e49
brnbp <bruno9pereira@gmail.com>
2016-11-09 00:57:59 -0200

    update webhook

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('done'));
 
-app.post('/webhook', (req, res) => res.send(req.body));
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 
 app.listen(process.env.PORT || 3000)
